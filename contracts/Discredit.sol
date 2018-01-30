791e759e2b67d11198c5348450531ee447b227f7
pryce06 <throttlehappy007@gmail.com>
2018-01-29 16:56:37 -0800

    Removed voter struct and replaced with voteType mapping. Getting 'TypeError: Indexed expression has to be a type, mapping or array (is uint256)         voteType[msg.sender] = 2;         ^------^' at compile, see stack exchange for potential answers

--- contracts/Discredit.sol
+++ contracts/Discredit.sol
@@ -5,18 +5,13 @@ import "./Vouch.sol";
 contract Discredit is Vouch {
 
 
-    struct Voter {
-        uint8 voteType;
-        bool voted;
-    }
-
     struct Count {
         uint voteReal;
         uint voteFake;
     }
     
     mapping (address => Count) public count;
-    mapping (address => Voter) public voters;
+    mapping (address => uint) public voteType;
     mapping (address => bool) public blacklist;
     uint numVoters;
    
@@ -37,29 +32,28 @@ contract Discredit is Vouch {
     }
 
     function castVote (uint voteType) canVote {
-        require (!(voters[msg.sender].voted));
-        if (voteType == 1) {//1 here meaning "Real" account.
+//        require (voteType[msg.sender] == 0);
+        if (voteType == 2) {//2 here meaning "Real" account.
         count[upForVote].voteReal++;
-        voters[msg.sender].voteType = 1;
+        voteType[msg.sender] = 2;
         }
-        if (voteType == 0) {//0 here meaning "Fake" acount.
+        if (voteType == 1) {//1 here meaning "Fake" acount.
         count[upForVote].voteFake++;
-        voters[msg.sender].voteType = 0;
+        voteType[msg.sender] = 1;
         }
-        voters[msg.sender].voted = true;
         voteInProgress[msg.sender] = true;
     }
 
     function executeOutcomes () { //rewrite all of this to fit new voters mapping
 //        require (block.number >= endTime);
         bool outcome = ((count[upForVote].voteReal) > (count[upForVote].voteFake));
-        if ((voters[msg.sender].voteType == 0) && (outcome == false)) { //Voted fake, account fake.
+        if ((voteType[msg.sender] == 1) && (outcome == false)) { //Voted fake, account fake.
             IdentityFundBalance[upForVote] -= standardDeposit;
             IdentityFundBalance[msg.sender] -= standardDeposit;
             balances[msg.sender] += (standardDeposit * 2); //Receives fake account's deposit and his own is returned.
             approvedConnections[msg.sender][upForVote] = false;
             approvedConnections[upForVote][msg.sender] = false;
-        } else if ((voters[msg.sender].voteType == 1) && (outcome == false) || (voters[msg.sender].voteType == 2) && (outcome == false)) { //Voted real or abstained, account fake.
+        } else if ((voteType[msg.sender] == 2) && (outcome == false) || (voteType[msg.sender] == 0) && (outcome == false)) { //Voted real or abstained, account fake.
             IdentityFundBalance[upForVote] -= standardDeposit;
             IdentityFundBalance[msg.sender] -= standardDeposit;
             balances[this] += (standardDeposit * 2);
@@ -67,24 +61,24 @@ contract Discredit is Vouch {
             approvedConnections[upForVote][msg.sender] = false;                
             isFrozen[upForVote] = true;
             blacklist[msg.sender] = true;
-        } else if ((voters[msg.sender].voteType == 0) && (outcome == true)) { //Voted fake, account real.
+        } else if ((voteType[msg.sender] == 1) && (outcome == true)) { //Voted fake, account real.
             IdentityFundBalance[msg.sender] -= standardDeposit;
             balances[upForVote] += standardDeposit;
             approvedConnections[msg.sender][upForVote] = false;
             approvedConnections[upForVote][msg.sender] = false;            
-        } else if ((voters[msg.sender].voteType == 2) && (outcome == true)) { //Voter abstained, account real.
+        } else if ((voteType[msg.sender] == 0) && (outcome == true)) { //Voter abstained, account real.
             disconnect(upForVote, msg.sender);
         } else {} //Voted real, account real.    
     }
 
-    function getVoteType (address target) view returns (uint) {
-        return voters[target].voteType;
+    function getVoteType (address target) view returns (uint8) {
+        return voteType[target];
     }
 
-    function getVoteCount (uint voteType) view returns (uint) {
-        if (voteType == 1)
+    function getVoteCount (uint8 voteType) view returns (uint) {
+        if (voteType == 2)
         return count[upForVote].voteReal;
-        if (voteType == 0)
+        if (voteType == 1)
         return count[upForVote].voteFake;
     }
 
